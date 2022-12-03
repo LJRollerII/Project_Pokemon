a85427b9ec1427d5b45654c2be0f43e9d4c4b143
LJRollerII <ljcoolkid@optonline.net>
2022-12-02 20:21:38 -0500

    Completed coding the pokemon class

--- scripts/pokemon_game.py
+++ scripts/pokemon_game.py
@@ -5,17 +5,17 @@ import sys
 # Delay Printing
 # Print one character at a time like the original Gameboy Version.
 
-def delay_prints(s):
+def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush
         time.sleep(0.05)
 
 class Pokemon:
-    def __init__(self, name, tpyes, moves, EVs, health='==================='):
+    def __init__(self, name, types, moves, EVs, health='==================='):
         # Save variables as attributes
         self.name = name
-        self.types = types
+        self.types = types 
         self.moves = moves
         self.attack = EVs['ATTACK']
         self.defense = EVs['DEFENSE']
@@ -67,6 +67,70 @@ class Pokemon:
                     Pokemon2.defense /= 2
                     string_1_attack = '\nIts super effective!'
                     string_2_attack = '\nIts not very effective...'
+
+        # Now for the actual fighting...
+        # Continue while pokemon still have health
+        while(self.bars > 0) and (Pokemon2.bars > 0):
+            # Print the health of each pokemon
+            print(f"\n{self.name}\t\tHLTH\t{self.health}")
+            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+
+            print(f"Go {self.name}!")
+            for i, x in enumerate(self.moves):
+                print(f"{i+1}.", x)
+            index = int(input('Pick a move: '))
+            delay_print(f"\n{self.name} used {self.moves[index-1]}!")
+            time.sleep(1)
+            delay_print(string_1_attack)
+
+            # Determine damage
+            Pokemon2.bars -= self.attack
+            Pokemon2.health = ""
+
+            # Add back bars plus defense boost
+            for j in range(int(Pokemon2.bars+.1*Pokemon2.defense)):
+                Pokemon2.health += "="
+
+            time.sleep(1)
+            print(f"\n{self.name}\t\tHLTH\t{self.health}")
+            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+            time.sleep(.5)
+
+            # Check to see if a pokemon fainted
+            if Pokemon2.bars <=0:
+                delay_print("\n..." + Pokemon2.name + ' fainted.')
+                break
+
+            # Pokemon2's turn
+
+            print(f"Go {Pokemon2.name}!")
+            for i, x in enumerate(Pokemon2.moves):
+                print(f"{i+1}.", x)
+            index = int(input('Pick a move: '))
+            delay_print(f"\n{Pokemon2.name} used {Pokemon2.moves[index-1]}!")
+            time.sleep(1)
+            delay_print(string_2_attack)
+
+            # Determine damage
+            self.bars -= Pokemon2.attack
+            self.health = ""
+
+            # Add back bars plus defense boost
+            for j in range(int(self.bars+.1*self.defense)):
+                self.health += "="
+
+            time.sleep(1)
+            print(f"{self.name}\t\tHLTH\t{self.health}")
+            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
+            time.sleep(.5)
+
+            # Check to see if Pokemon fainted
+            if self.bars <= 0:
+                delay_print("\n..." + self.name + ' fainted.')
+                break
+
+            money = np.random.choice(5000)
+            delay_print(f"\nOpponent paid you ${money}.\n")
 
 
 
